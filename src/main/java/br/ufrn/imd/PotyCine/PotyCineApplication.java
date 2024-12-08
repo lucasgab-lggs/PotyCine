b2efcba36e54ed44f954055c72ef8483a7c2ca2b
Lucas Gabriel <lucasgab.lggs@gmail.com>
2024-12-08 14:28:33 -0300

    feat: criar projeto inicial Spring com estrutura básica

---
+++ src/main/java/br/ufrn/imd/PotyCine/PotyCineApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.PotyCine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotyCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotyCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/PotyCine/PotyCineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.PotyCine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotyCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
